f1bf18504224210b4bbeeb53dc3c3b678d440dc6
IronCoreZkt <yexinche@gmail.com>
2019-07-17 15:34:43 +0800

    修改笔记备注

--- src/com/chapter8/CodeKeeper.java
+++ src/com/chapter8/CodeKeeper.java
@@ -5,9 +5,10 @@ import java.util.Iterator;
 
 /**
  * ArrayList测试，主要关于contain,add操作。
+ * 
  * @author Iron Core
  * @date 2019年7月15日
- * @note 
+ * @note ArrayList超过指定容量的时候，数组列表会自动增长，增加的长度为旧（增加前）容量的一半，{@link ArrayList#grow}。
  * 
  */
 public class CodeKeeper {
