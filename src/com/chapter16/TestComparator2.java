560c6ef18c7402012b227edb46931ca5c863b135
IronCoreZkt <yexinche@gmail.com>
2019-07-30 16:42:41 +0800

    补充说明，lambda表达式不能实现抽象类

--- src/com/chapter16/TestComparator2.java
+++ src/com/chapter16/TestComparator2.java
@@ -7,6 +7,7 @@ package com.chapter16;
  * @date 2019年7月30日
  * @note {@see Comparator}中的equals方法不需要实现，因为所有类默认继承Object的equals方法，相当于已经重写。
  * 		 lambda表达式实现的类型必须是函数式接口functional interface，也就是只有一个方法的接口。
+ *       （lambda表达式实现的类型不能是抽象类，但内部类可以）。
  * 
  */
 public class TestComparator2 implements TestComparator{
