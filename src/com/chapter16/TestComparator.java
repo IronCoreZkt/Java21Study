60a2192c4f3e7128b5d2fd3adeb4e43cebafcc70
IronCoreZkt <yexinche@gmail.com>
2019-07-30 16:36:00 +0800

    测试lambda的实现类型和限制还有Comparator#equals方法不用实现的原因

---
+++ src/com/chapter16/TestComparator.java
@@ -0,0 +1,19 @@
+package com.chapter16;
+
+//@FunctionalInterface
+/**
+ * 配合测试lambda的实现类型和限制还有Comparator#equals方法不用实现的原因
+ * 
+ * @author Iron Core
+ * @date 2019年7月30日
+ * @note {@see Comparator}中的equals方法不需要实现，因为所有类默认继承Object的equals方法，相当于已经重写。
+ * 		 lambda表达式实现的类型必须是函数式接口functional interface，也就是只有一个方法的接口。
+ * 
+ */
+public interface TestComparator {
+	void show();
+	// void bark();
+
+	boolean equals(Object obj);
+
+}
---
+++ src/com/chapter16/TestComparator2.java
@@ -0,0 +1,40 @@
+package com.chapter16;
+
+/**
+ * 测试lambda的实现类型和限制还有Comparator#equals方法不用实现的原因
+ * 
+ * @author Iron Core
+ * @date 2019年7月30日
+ * @note {@see Comparator}中的equals方法不需要实现，因为所有类默认继承Object的equals方法，相当于已经重写。
+ * 		 lambda表达式实现的类型必须是函数式接口functional interface，也就是只有一个方法的接口。
+ * 
+ */
+public class TestComparator2 implements TestComparator{
+	@Override
+	public void show() {
+		/*TestComparator tc = new TestComparator(){
+			public void show() {
+				System.out.println("内部类中的实现");
+			}
+
+			@Override
+			public void bark() {
+				// TODO 自动生成的方法存根
+				
+			}
+		};*/
+		TestComparator tc = ()->{
+				System.out.println("内部类中的实现");
+			};
+		tc.show();
+	}
+
+	/*@Override
+	public void bark() {
+		// TODO 自动生成的方法存根
+		
+	}*/
+	public static void main(String[] args){
+		new TestComparator2().show();
+	}
+}
