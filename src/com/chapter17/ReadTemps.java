faf70d3b1db67873f687acc45b20fdd969176468
IronCoreZkt <yexinche@gmail.com>
2019-09-10 10:00:25 +0800

    调整代码和注释，更加体现Buffer类的限制limit和当前位置position。

--- src/com/chapter17/ReadTemps.java
+++ src/com/chapter17/ReadTemps.java
@@ -1,5 +1,6 @@
 package com.chapter17;
 
+import java.nio.Buffer;
 import java.nio.IntBuffer;
 
 /**
@@ -7,8 +8,9 @@ import java.nio.IntBuffer;
  * 
  * @author Iron Core
  * @date 2019年9月9日
- * @note nio包中buff的put(int[] src)会覆盖当前位置(position)到该数组src的长度（长度超过缓冲区大小会报错），
+ * @note nio包中Buffer的put(int[] src)会覆盖当前位置(position)到该数组src的长度（长度超过缓冲区大小会报错），
  *       然后将当前位置标记到src长度+1的位置。
+ *       实际上Buffer类还有标记mark和容量capacity，mark<=position<=limit<=capacity，但在这里我们不关注它们。
  *
  */
 public class ReadTemps {
@@ -17,15 +19,24 @@ public class ReadTemps {
 		int[] temps = { 25, 26, 27, 28, 29, 30 };
 		IntBuffer buff = IntBuffer.wrap(temps);
 		int[] autumn = { 20, 21, 22 };
+		showCurrentLimitAndPosition(buff);
 		buff.put(autumn);
-		//buff.flip();//flip刷新了缓冲区，并将限制limit设置为当前位置，28，29，30会被丢弃。
-		buff.position(0);
-		while (buff.hasRemaining())
-			System.out.println("First position's content is " + buff.get() + ".");
+		showCurrentLimitAndPosition(buff);
+		buff.flip();//flip刷新了缓冲区，并将限制limit设置为当前位置，28，29，30会被丢弃。
+		showCurrentLimitAndPosition(buff);
+		//buff.position(0);
+		for (int i =0;buff.hasRemaining();i++)
+			System.out.println("The "+i+" position's content is " + buff.get() + ".");
+		showCurrentLimitAndPosition(buff);
 	}
 
 	public static void main(String[] args) {
 		new ReadTemps();
+	}
+	
+	public static final void showCurrentLimitAndPosition(Buffer buff){
+		System.out.println("此时限制\t\tlimit="+buff.limit());
+		System.out.println("此时当前位置\tposition="+buff.position());
 	}
 
 }
